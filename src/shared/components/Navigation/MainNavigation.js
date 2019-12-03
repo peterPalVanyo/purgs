21a27472fa0b0c3d0347cca5b66150f94cb16d1b
peterPalVanyo <vanyo.peter.pal@gmail.com>
2019-12-03 14:22:17 +0100

    fix classpass bug

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -4,27 +4,30 @@ import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
-import Backdrop from '../UIElement/Backdrop'
+import Backdrop from "../UIElement/Backdrop";
 
 const MainNavigation = props => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
   const openDrawerHandler = () => {
-      setDrawerIsOpen(true)
-  }
+    setDrawerIsOpen(true);
+  };
   const closeDrawerHandler = () => {
-      setDrawerIsOpen(false)
-  }
+    setDrawerIsOpen(false);
+  };
   return (
     <React.Fragment>
-    {drawerIsOpen && <Backdrop onClick={closeDrawerHandler}/>}
-        <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
-          <nav className="main-navigation__drawer-nav">
-            <NavLinks />
-          </nav>
-        </SideDrawer>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
       <MainHeader>
         {/* for the hamburger */}
-        <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
           <span />
           <span />
           <span />
