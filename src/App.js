9b46c99e5ef0c56310335710417e9184cdc66035
peterPalVanyo <vanyo.peter.pal@gmail.com>
2019-12-03 09:29:49 +0100

    navigation menu and mobile view

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import Users from "./users/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Users />
-        </Route>
-        <Route exact path="/new">
-          <NewPlace />
-        </Route>
-      </Switch>
+      <main>
+        <MainNavigation />
+        <Switch>
+          <Route exact path="/">
+            <Users />
+          </Route>
+          <Route exact path="/places/new">
+            <NewPlace />
+          </Route>
+        </Switch>
+      </main>
     </BrowserRouter>
   );
 }
--- src/users/components/UsersList.js
+++ src/users/components/UsersList.js
@@ -1,23 +1,26 @@
 import React from "react";
 import "./UsersList.css";
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElement/Card";
 
 const UsersList = props => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users here.</h2>
+        <Card>
+          <h2>No users here.</h2>
+        </Card>
       </div>
     );
   }
   return (
-    <ul className='users-list'>
+    <ul className="users-list">
       {props.items.map(item => (
         <UserItem
           key={item.id}
           id={item.id}
           image={item.image}
-          name={item.name} 
+          name={item.name}
           places={item.places}
         />
       ))}
